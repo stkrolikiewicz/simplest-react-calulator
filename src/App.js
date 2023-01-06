090f3203096fe70341e64c27643b3b4b763b455a
stachkrol <st.krolikiewicz@gmail.com>
2023-01-06 09:43:15 +0100

    styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useState, useRef } from "react";
 import "./App.css";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { solid, regular } from "@fortawesome/fontawesome-svg-core/import.macro";
+// import { regular } from "@fortawesome/fontawesome-svg-core";
 function App() {
     const inputRef = useRef(null);
     const resultRef = useRef(null);
@@ -54,12 +56,18 @@ function App() {
                     type="number"
                     placeholder="Type a number"
                 />
-                <button onClick={plus}>add</button>
-                <button onClick={minus}>substract</button>
-                <button onClick={times}>multiply</button>
-                <button onClick={divide}>divide</button>
-                <button onClick={resetInput}>reset Input</button>
-                <button onClick={resetResult}>reset Result</button>
+                <div className="buttons">
+                    <button onClick={plus}>
+                        <FontAwesomeIcon icon={solid("plus")} />
+                    </button>
+                    <button onClick={minus}>
+                        <FontAwesomeIcon icon={regular("coffee")} />
+                    </button>
+                    <button onClick={times}>x</button>
+                    <button onClick={divide}>:</button>
+                    <button onClick={resetInput}>reset Input</button>
+                    <button onClick={resetResult}>reset Result</button>
+                </div>
             </form>
         </div>
     );
